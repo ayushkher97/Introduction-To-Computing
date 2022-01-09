991ec1fb80d00cb353930f4964622af22f421d0f
ayushkher97 <97087956+ayushkher97@users.noreply.github.com>
2022-01-09 20:34:57 +0530

    Add files via upload

---
+++ Output_Assignment1.py
@@ -0,0 +1,50 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============== RESTART: C:\Users\HP\OneDrive\Desktop\question1.py ==============
+First Question
+Enter your first number:10.1
+Enter your second number:10.1
+Enter your third number:10.1
+The average of the three numbers is: 10.1
+
+
+================================================================== RESTART: C:\Users\HP\OneDrive\Desktop\Question 2.py =================================================================
+Second Question
+What is your gross income?3600000
+What is the number of dependants?4
+The Taxable Income Is: 3578000.0
+The Total Amount Of Tax Is: 715600.0
+
+
+================================================================== RESTART: C:\Users\HP\OneDrive\Desktop\Question3.py ==================================================================
+Third Question
+List of the students
+Enter the SID:21105001
+Enter the name:Ayush Kher
+Enter the gender.
+(M,F,U)         :M
+Enter the course name.
+(CSE,ECE,EE,ME,,CE,AERO,META):ECE
+Enter the CGPA:9.5
+[SID,NAME,GENDER,COURSENAME,CGPA]
+List Of The Student Is:
+[21105001, 'Ayush Kher', 'M', 'ECE', 9.5]
+
+
+================================================================== RESTART: C:\Users\HP\OneDrive\Desktop\Question4.py ==================================================================
+Fourth Question
+Enter the marks of the first student:99
+Enter the marks of the second student:98
+Enter the marks of the third student:97
+Enter the marks of the fourth student:96
+Enter the marks of the fifth student:95
+List of the student's marks: [99.0, 98.0, 97.0, 96.0, 95.0]
+List of the student's marks (sorted): [95.0, 96.0, 97.0, 98.0, 99.0]
+
+
+================================================================== RESTART: C:\Users\HP\OneDrive\Desktop\Question5.py ==================================================================
+Fifth Question
+Original list of the color is: ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+List of the colors after removing black color is: ['Red', 'Green', 'White', 'Pink', 'Yellow']
+List of the colors after replacing black and pink by purple ['Red', 'Green', 'White', 'Purple', 'Yellow']
