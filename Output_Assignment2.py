656099b78e69311455062726d49844360927948d
ayushkher97 <97087956+ayushkher97@users.noreply.github.com>
2022-01-22 00:12:09 +0530

    Add files via upload

---
+++ Output_Assignment2.py
@@ -0,0 +1,51 @@
+
+SOLUTION-1
+
+(a)The Length of the given string is: 35
+(b)The string in the reverse order is: egaugnal evitisnes esac a si nohtyP
+(c)The new string becomes: a case sensitive
+(d)The input string after replacing: Python is object oriented language
+(e)The index of the substring 'a' is: 10
+(f)The input string after removing the white spaces is: Pythonisacasesensitivelanguage
+
+
+SOLUTION-2
+
+Enter your name:Ayush Kher
+Enter your SID:21105001
+Enter the name of your department:ECE
+Enter your CGPA:9.9
+Hey,Ayush Kher Here! 
+ My SID is 21105001 
+ I am from ECE department and my cgpa is 9.900000.
+
+
+SOLUTION-3
+
+(a) a&b=8
+(b) a|b=58
+(c) a^b=50
+(d) Left shift both a and b with 2 bits: a=224,b=40
+(e) Right shifth awith 2 bits and b with 4 bits: a=14,b=0
+
+
+SOLUTION-4
+
+Enter the first number:10
+Enter the second number:11
+Enter the third number:12
+The greatest number is: 12
+
+
+SOLUTION-5
+
+Enter the string:Ekaksh is a good name
+Yes
+
+
+SOLUTION-6
+
+Enter the length of first side of the triangle:5
+Enter the length of second side of the triangle:12
+Enter the length of third side of the triangle:13
+Yes
