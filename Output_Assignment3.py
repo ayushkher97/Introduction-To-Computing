350c897955a3934d32c78edea5c721f8f9135c34
ayushkher97 <97087956+ayushkher97@users.noreply.github.com>
2022-02-13 11:12:19 +0530

    Add files via upload

---
+++ Output_Assignment3.py
@@ -0,0 +1,127 @@
+Question1
+
+Enter your string:Python is widely used in the world of data science so it is an important tool for anyone getting started in data science.
+Number of each word/character is: {'python': 1, 'is': 2, 'widely': 1, 'used': 1, 'in': 2, 'the': 1, 'world': 1, 'of': 1, 'data': 2, 'science': 1, 'so': 1, 'it': 1, 'an': 1, 'important': 1, 'tool': 1, 'for': 1, 'anyone': 1, 'getting': 1, 'started': 1, 'science.': 1}
+
+
+Question 2
+
+Enter the date
+[1,31]		:31
+Enter the month
+[1,12]		:3
+Enter the year
+[1800,2025]	:2025
+
+Next Date Is: 1/4/2025
+
+
+Question 3
+
+ Enter the total number of numbers that you are going to enter: 5
+
+ Enter the 1 number:1
+ Enter the 2 number:2
+ Enter the 3 number:3
+ Enter the 4 number:4
+ Enter the 5 number:5
+
+The Final Output List Is: [(1, 1), (2, 4), (3, 9), (4, 16), (5, 25)]
+
+
+Question 4
+
+Enter the grade points 
+ [4,10] 		 :9
+
+Your Grade is 'A' and Excellent Performance.
+
+
+Question 5
+
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+
+Question 6
+
+Do you want to enter detials of student? 
+		(Y or N) 		: Y
+
+Enter the student's SID:21105001
+Enter the student's name: Ayush Kher
+
+Do you want to enter detials of student? 
+		(Y or N) 		: Y
+
+Enter the student's SID:21105034
+Enter the student's name: Aaryan Abbot
+
+Do you want to enter detials of student? 
+		(Y or N) 		: Y
+
+Enter the student's SID:21105093
+Enter the student's name: Akshat Dutta
+
+Do you want to enter detials of student? 
+		(Y or N) 		: N
+
+Part a
+
+Student detials are:  {21105001: 'Ayush Kher', 21105034: 'Aaryan Abbot', 21105093: 'Akshat Dutta'}
+
+Part b
+
+Sorted dictionary using student's name is: {21105034: 'Aaryan Abbot', 21105093: 'Akshat Dutta', 21105001: 'Ayush Kher'}
+
+Part c
+
+Sorted dictionary using student's SID is:  {21105001: 'Ayush Kher', 21105034: 'Aaryan Abbot', 21105093: 'Akshat Dutta'}
+
+Part d
+
+Enter the SID whose name you want to search: 21105001
+
+Student's name is: Ayush Kher
+
+
+Question 7
+
+Enter the the number of terms you want in the sequence: 6
+
+Fibonacci sequence with 6 terms:
+0
+1
+1
+2
+3
+5
+
+Average of fibonacci sequence is: 2.0
+
+
+Question 8
+
+Part a
+
+Set of all the elemets that are in Set 1 and Set 2 but not in both: {1, 3, 5, 6, 8}
+
+Part b
+
+Set of all the elements that are only in one of the three sets (Set1,Set2,Set3): {3, 6, 8, 9, 13, 17}
+
+Part c
+
+Set of all the elements that are exactly in two of the sets (Set1, Set2 and Set3): {1, 2, 4, 5}
+
+Part d
+
+Set of all the integers in the range 1 to 10 that are not in set_1:  {6, 7, 8, 9, 10}
+
+Part e
+
+The set having all integers less than equal to 10 except that are not in all the sets:  {10, 7}
